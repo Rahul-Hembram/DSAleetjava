6efc1f0dda35dab72767769416bb1269d232120c
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-18 09:49:31 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 700455--GFG/700455-.java
@@ -0,0 +1,41 @@
+class Solution {
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+    public void threeWayPartition(int arr[], int a, int b) {
+        // code here
+        int start = 0;
+        int end = 0;
+
+        while(end < arr.length){
+            if(arr[end] >= a){
+                end++;
+            }
+            else{
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end++;
+            }
+        }
+
+        end = start;
+
+         while(end < arr.length){
+            if(arr[end] >= b){
+                end++;
+            }
+            else{
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end++;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
