8545906fd4d2e9946986d3e442e81563b931932a
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-19 08:27:52 +0530

    Force committed solution

---
+++ 712390--GFG/712390-.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canServe(int[] arr) {
+        int five = 0;
+        int ten = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == 5) {
+                five++;
+            }
+
+            else if (arr[i] == 10) {
+                if (five == 0) {
+                    return false;
+                }
+
+                five--;
+                ten++;
+            }
+
+            else if (arr[i] == 20) {
+                // Prefer giving 10 + 5
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+
+                // Otherwise give 5 + 5 + 5
+                else if (five >= 3) {
+                    five -= 3;
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
