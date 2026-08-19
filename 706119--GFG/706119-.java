20e6f561e2038dd35015a44b8ef1d08e8dc82094
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-19 08:22:24 +0530

    Added solution

---
+++ 706119--GFG/706119-.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+
+    long countT(int[] arr, int n, long x) {
+        long ans = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                long sum = (long) arr[i] + arr[j] + arr[k];
+
+                if (sum <= x) {
+                    ans += (k - j);
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    long countTriplets(int[] arr, int l, int r) {
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        return countT(arr, n, r) - countT(arr, n, (long) l - 1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
