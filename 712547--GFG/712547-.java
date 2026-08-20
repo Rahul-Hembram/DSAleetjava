0e9fadc3d86e915ad122be68408f3a6f6879a45f
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-20 13:30:52 +0530

    Added solution

---
+++ 712547--GFG/712547-.java
@@ -0,0 +1,12 @@
+class Solution {
+    static long count(int n) {
+    // code here
+        int totalEdges = n * (n - 1) / 2;
+        // Total graphs = 2^(totalEdges)
+        return (long) Math.pow(2, totalEdges);
+  }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
