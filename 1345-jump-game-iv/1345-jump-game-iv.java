67959423cf722a50c5a2c00acb0e6097f4e64072
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-21 11:21:07 +0530

    Time: 61 ms (51.48%), Space: 90.2 MB (33.06%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,49 +1,46 @@
-import java.util.*;
-
 class Solution {
     public int minJumps(int[] arr) {
         int n = arr.length;
-        if (n == 1) return 0;
-
-        Map<Integer, List<Integer>> mp = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            mp.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+        
+        if(n == 1) return 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int step = 0;
+        
+        // fill the map
+        for(int i = 0; i < n; i++){
+            map.computeIfAbsent(arr[i], v -> new ArrayList()).add(i);
         }
-
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-
-        boolean[] vis = new boolean[n];
-        vis[0] = true;
-
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int node = curr[0];
-            int dist = curr[1];
-
-            if (node == n - 1) return dist;
-
-            if (node - 1 >= 0 && !vis[node - 1]) {
-                vis[node - 1] = true;
-                q.offer(new int[]{node - 1, dist + 1});
-            }
-
-            if (node + 1 < n && !vis[node + 1]) {
-                vis[node + 1] = true;
-                q.offer(new int[]{node + 1, dist + 1});
-            }
-
-            for (int next : mp.get(arr[node])) {
-                if (!vis[next]) {
-                    vis[next] = true;
-                    q.offer(new int[]{next, dist + 1});
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        
+        while(!q.isEmpty()){
+            step++;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                int j = q.poll();
+            
+                if(j - 1 >= 0 && map.containsKey(arr[j - 1])){
+                    q.offer(j - 1);
                 }
+
+                if(j + 1 < n && map.containsKey(arr[j + 1])){
+                    if(j + 1 == n - 1) return step;
+                    q.offer(j + 1);
+                }
+
+                if(map.containsKey(arr[j])){
+                    for(int k : map.get(arr[j])){
+                        if(k != j){
+                            if(k == n - 1) return step;
+                            q.offer(k);
+                        }
+                    }
+                }
+                map.remove(arr[j]);
             }
-
-            mp.get(arr[node]).clear();
         }
-
-        return -1;
+        
+        return step;
     }
 }
 
