da36d497cb3aebb0fc234b8f87b61fb6c84f4f7b
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-22 10:41:51 +0530

    Force committed solution

---
+++ 914136--GFG/914136-.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> matToAdj(int[][] mat) {
+        // code here
+
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        for(int i = 0;i<mat.length;i++){
+            ArrayList<Integer> a = new ArrayList<>();
+            for(int j = 0;j<mat.length;j++){
+                if(mat[i][j]==1) a.add(j);
+            }
+            arr.add(a);
+        }
+        return arr;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
