32fde4289990847a53926ae8846c9f660aad6323
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-23 09:27:05 +0530

    Time: 1 ms (47.78%), Space: 125.6 MB (11.22%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        if (n <= 2) return n;
+
+        int mask = 0;
+        for (int num : nums) {
+            mask |= num;
+        }
+
+        return mask + 1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
