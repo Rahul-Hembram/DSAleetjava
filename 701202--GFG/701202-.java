ac32312e367a66edc6d295948a1aa21f63c7f211
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-24 08:16:49 +0530

    Added solution

---
+++ 701202--GFG/701202-.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ArrayList<Integer> frequencyCount(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int x : arr) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            ans.add(map.getOrDefault(i, 0));
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
