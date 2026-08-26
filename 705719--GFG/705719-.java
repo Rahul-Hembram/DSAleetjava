398e47641760511d148f5db190a333fb291c825f
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-26 09:35:48 +0530

    Force committed solution

---
+++ 705719--GFG/705719-.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public boolean isNegativeWeightCycle(int n, int[][] edges) {
+        int INF = (int)1e9;
+        int[][] dist = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            dist[u][v] = Math.min(dist[u][v], w);
+        }
+
+        // Floyd-Warshall
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                if (dist[i][k] == INF) continue;
+
+                for (int j = 0; j < n; j++) {
+                    if (dist[k][j] == INF) continue;
+
+                    dist[i][j] = Math.min(
+                        dist[i][j],
+                        dist[i][k] + dist[k][j]
+                    );
+                }
+            }
+        }
+
+        // Check negative cycle
+        for (int i = 0; i < n; i++) {
+            if (dist[i][i] < 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
