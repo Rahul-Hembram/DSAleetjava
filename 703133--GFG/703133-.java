3b2e409ff93baa8fa4362b169129519313d50e1e
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-27 09:00:51 +0530

    Force committed solution

---
+++ 703133--GFG/703133-.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int maxArea(int[][] mat) {
+        int r = mat.length;
+        int c = mat[0].length;
+
+        int result = 0;
+        int[] height = new int[c];
+
+        for (int i = 0; i < r; i++) {
+
+            for (int j = 0; j < c; j++) {
+                if (mat[i][j] == 0)
+                    height[j] = 0;
+                else
+                    height[j]++;
+            }
+
+            int[] arr = height.clone();
+            Arrays.sort(arr);
+
+            for (int j = 0; j < c; j++) {
+                result = Math.max(result, arr[j] * (c - j));
+            }
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
