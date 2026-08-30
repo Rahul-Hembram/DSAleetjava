0abda9da99325732e42b42dc24b70b90ebae31e8
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-08-30 09:59:24 +0530

    Force committed solution - Time: O(n + m log n), Space: O(n + m)

---
+++ 702846--GFG/702846-.java
@@ -0,0 +1,43 @@
+class Solution {
+    public ArrayList<Integer> getMarks(int[] l, int[] r, int[] rank) {
+        int n = l.length;
+
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + (r[i] - l[i]) + 1;
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for (int x : rank) {
+            int pos = lowerBound(prefix, x);
+            int offset = x - prefix[pos - 1] - 1;
+            res.add(l[pos - 1] + offset);
+        }
+
+        return res;
+    }
+
+    private int lowerBound(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
