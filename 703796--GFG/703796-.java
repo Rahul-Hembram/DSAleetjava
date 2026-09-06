cdb10e288d5895d81121ee7bd89a53b7442b646b
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-09-06 09:13:02 +0530

    Added solution - Time: O(32n), Space: O(1)

---
+++ 703796--GFG/703796-.java
@@ -0,0 +1,23 @@
+class Solution {
+    static long pairAndSum(int arr[]) {
+        long ans = 0;
+
+        for (int bit = 0; bit < 32; bit++) {
+            long count = 0;
+
+            for (int x : arr) {
+                if ((x & (1L << bit)) != 0) {
+                    count++;
+                }
+            }
+
+            ans += (count * (count - 1) / 2) * (1L << bit);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
