492df8ff95bf93d5ad7d95493dbd85e3f8456cd3
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-09-07 07:47:07 +0530

    Time: 2 ms (100%), Space: 43.4 MB (41.84%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+        final long MOD = 1000000007L;
+        
+        long dp = 1;
+        long[] last = new long[26];
+        
+        for (char c : s.toCharArray()) {
+            int index = c - 'a';
+            
+            long oldDp = dp;
+            
+            dp = (2 * dp - last[index] + MOD) % MOD;
+            
+            last[index] = oldDp;
+        }
+        
+        return (int)((dp - 1 + MOD) % MOD);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
