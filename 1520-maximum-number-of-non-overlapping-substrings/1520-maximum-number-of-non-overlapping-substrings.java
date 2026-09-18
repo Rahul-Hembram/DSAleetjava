71fafe9d097be90760c38796084010f3223e8b19
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-09-18 08:19:22 +0530

    Time: 173 ms (5.63%), Space: 48.5 MB (26.09%) - LeetHub - Time: O(N), Space: O(N)

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -1,48 +1,68 @@
 class Solution {
+
     public List<String> maxNumOfSubstrings(String s) {
-        int[] first = new int[26];
-        int[] last = new int[26];
-        Arrays.fill(first, -1);
-        for (int i = 0; i < s.length(); i++) {
-            int c = s.charAt(i) - 'a';
-            if (first[c] == -1) {
-                first[c] = i;
-            }
-            last[c] = i;
+        Seg[] seg = new Seg[26];
+        for (int i = 0; i < 26; ++i) {
+            seg[i] = new Seg(-1, -1);
         }
-        List<String> res = new ArrayList<>();
-        int right = -1;
-        for (int i = 0; i < s.length(); i++) {
-            if (i == first[s.charAt(i) - 'a']) {
-                int newRight = checkValidRightBoundary(s, i, first, last);
-                
-                if (newRight != -1) {
-                   
-                    if (i > right) {
-                        res.add("");
+        // Preprocess the left and right endpoints.
+        for (int i = 0; i < s.length(); ++i) {
+            int charIdx = s.charAt(i) - 'a';
+            if (seg[charIdx].left == -1) {
+                seg[charIdx].left = seg[charIdx].right = i;
+            } else {
+                seg[charIdx].right = i;
+            }
+        }
+        for (int i = 0; i < 26; ++i) {
+            if (seg[i].left != -1) {
+                for (int j = seg[i].left; j <= seg[i].right; ++j) {
+                    int charIdx = s.charAt(j) - 'a';
+                    if (
+                        seg[i].left <= seg[charIdx].left &&
+                        seg[charIdx].right <= seg[i].right
+                    ) {
+                        continue;
                     }
-                    
-                    right = newRight;
-                    res.set(res.size() - 1, s.substring(i, right + 1));
+                    seg[i].left = Math.min(seg[i].left, seg[charIdx].left);
+                    seg[i].right = Math.max(seg[i].right, seg[charIdx].right);
+                    j = seg[i].left;
                 }
             }
         }
-        
-        return res;
-    }
-    
-    public int checkValidRightBoundary(String s, int i, int[] first, int[] last) {
-        int right = last[s.charAt(i) - 'a'];
-        for (int j = i; j <= right; j++) {
-            int c = s.charAt(j) - 'a';
-            if (first[c] < i) {
-                return -1;
+        // Greedily select intervals.
+        Arrays.sort(seg);
+        List<String> ans = new ArrayList<>();
+        int end = -1;
+        for (Seg segment : seg) {
+            int left = segment.left,
+                right = segment.right;
+            if (left == -1) {
+                continue;
             }
-            
-           right = Math.max(right, last[c]);
+            if (end == -1 || left > end) {
+                end = right;
+                ans.add(s.substring(left, right + 1));
+            }
         }
-        
-        return right;
+        return ans;
+    }
+
+    class Seg implements Comparable<Seg> {
+
+        int left, right;
+
+        public Seg(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public int compareTo(Seg rhs) {
+            if (right == rhs.right) {
+                return rhs.left - left;
+            }
+            return right - rhs.right;
+        }
     }
 }
 
