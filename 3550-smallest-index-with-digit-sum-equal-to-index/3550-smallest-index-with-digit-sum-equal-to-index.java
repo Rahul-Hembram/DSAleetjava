375a5adfcdde2742c30dcec7a39955dfeb289135
Rahul Hembram <rahul.240101244@iiitbh.ac.in>
2026-09-24 09:55:47 +0530

    Time: 2 ms (2.26%), Space: 46.3 MB (5.14%) - LeetHub - Time: O(n * log n), Space: O(log n)

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            String s = "" + nums[i];
+
+            for (int j = 0; j < s.length(); j++) {
+                sum += s.charAt(j) - '0';
+            }
+
+            if (sum == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
